5d5b27d8f6654eff7c178a42de083949999d5907
Deyan Ardi <riyan.clsg11@gmail.com>
2020-09-25 19:42:26 +0800

    Project Layout Fixed

---
+++ lib/main.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Profile Saya'),
+          centerTitle: true,
+          leading: Icon(
+            Icons.favorite,
+            color: Colors.pink,
+            size: 30,
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Picture(),
+              TextName(),
+              TextWeb(),
+              RowCardOne(),
+              RowCardTwo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Picture extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CircleAvatar(
+        radius: 95,
+        backgroundColor: Colors.lightBlue,
+        child: CircleAvatar(
+          radius: 90,
+          backgroundImage: AssetImage('images/deyan.jpg'),
+        ),
+      ),
+      margin: const EdgeInsets.only(top: 40.0),
+    );
+  }
+}
+
+class TextName extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'I GEDE RIYAN ARDI DARMAWAN',
+        style: TextStyle(
+          color: Colors.blue,
+          fontSize: 23,
+        ),
+      ),
+      margin: const EdgeInsets.only(top: 20.0),
+    );
+  }
+}
+
+class TextWeb extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'https://debly.cc',
+        style: TextStyle(
+          color: Colors.blue,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
+
+class RowCardOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          width: 150,
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.school,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Undiksha',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          width: 150,
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.location_on,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Singaraja',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class RowCardTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          width: 150,
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Ngoding',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          width: 150,
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.music_video,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'All Genre',
+                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
